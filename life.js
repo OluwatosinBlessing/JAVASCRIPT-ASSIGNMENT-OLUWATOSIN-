e89259af5c420a7c62177da5d7484f989cc463f9
Ajayi Oluwatosin Blessing <101432229+OluwatosinBlessing@users.noreply.github.com>
2022-06-23 15:52:26 +0000

    Initial commit

---
+++ life.js
@@ -0,0 +1,19 @@
+alert('My Profile')
+
+alert('My Profile');
+    console.log('My Profile')
+
+let myName = "Ajayi Oluwatosin Blessing"
+console.log(myName);
+
+let myHeight = "5.9m"
+console.log(myHeight);
+
+let myCountry = "Nigeria"
+console.log(myCountry);
+
+
+
+document.write(myName);
+document.write(myHeight);
+document.write(myCountry);
